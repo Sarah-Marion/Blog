c1113ed77a4a883a7e840c654bc30b8e8a9cf972
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-26 15:29:41 +0300

    created an authentication routing function for loging in

---
+++ app/auth/views.py
@@ -0,0 +1,24 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import UserMixin, current_user, login_user, logout_user
+from .import auth
+from ..import db
+from ..models import User
+from .forms import LoginForm, RegisterForm
+
+
+@auth.route('/login',methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    title = "Login"
+    Login = LoginForm()
+
+    if Login.validate_on_submit:
+        user = User.query.filter_by(email = Login.email.data).first()
+        if user is not None and user.verify_password(Login.password.data):
+            login_user(user, Login.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid, Try again')
+        
+    return render_template('auth/login.html', Login = Login, title=title)
