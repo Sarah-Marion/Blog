025e9c8cbbb97bd88947dc91819b835075c49534
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-26 15:01:44 +0300

    created routing for a user to post their articles on to

--- app/main/views.py
+++ app/main/views.py
@@ -31,3 +31,41 @@ def index():
         
     return render_template('index.html', title = title, posts=all, subscribers=subscribers)
     
+
+
+@main.route('/profile/<username>')
+@login_required
+
+def profile(username):
+    
+    user = User.query.filter_by(username = username).first_or_404()
+    title = "Profile"       
+    return render_template('profile.html' , user=user,title=title)
+
+@main.route('/post', methods=['GET', 'POST'])
+def post():
+    all = Post.query.all()
+    all.reverse()
+    print(all)
+
+    Comments = CommentForm()
+    if Comments.validate_on_submit():
+        comment = Comment(comment = Comments.comment.data, commenter = Comments.commenter.data)
+        db.session.add(comment)
+        db.session.commit()
+        print(comment)
+        return redirect(url_for('main.post'))
+
+    allcomments = Comment.query.all()
+    title = "Post Article"
+    Blog = PostForm()
+    try:
+        if Blog.validate_on_submit():
+            post = Post( title = Blog.title.data ,post = Blog.Entry.data, author = current_user, timeposted = datetime.utcnow() )
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('main.post'))
+    except:
+        flash("Sorry you can NOT post more than 225 characters for now. We are aware of this situation and are currently working on this.")
+        return redirect(url_for('main.post'))
+            
