7407976eb9f03ecdd3e3e5e37eeda949c9e26816
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-26 13:07:50 +0300

    added the configurations needed for the applicaion

---
+++ config.py
@@ -0,0 +1,76 @@
+import os
+
+# Class-based application configuration
+class Config:
+    """
+    General configuration parent class
+    """
+    #pass
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY="5047aa27156e4b7f0a8c7dd7dc6ef2afa449fbd0b78226bfd538d03a6f2d2514b5dea9412baff14ff535f6f9b1db6e9260c6"
+    DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:postgres@localhost/pblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
+
+
+    #Flask-Mail SMTP server settings
+    MAIL_SERVER ='smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    BASIC_AUTH_USERNAME = 'Sarah'
+    BASIC_AUTH_pASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+    
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with General
+        configuration settings
+    """
+    # pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG = True
+
+
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General
+        configuration settings
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG = True
+
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+
+config_options = {
+    'development' : DevConfig,
+    'production' : ProdConfig,
+    'test' : TestConfig
+}
