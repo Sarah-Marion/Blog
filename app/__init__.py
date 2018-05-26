c00b1f5a6a4a6f048938d76e2295af13c38eafce
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-26 13:27:40 +0300

    created the app configurations and initialized the flask extensions

---
+++ app/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_admin import Admin
+from flask_simplemde import SimpleMDE
+from flask_basicauth import BasicAuth
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+admin = Admin()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
+basic_auth= BasicAuth()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations.
+    app.config.from_object(config_options[config_name])
+
+    # Intitializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    admin.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+    basic_auth.init_app(app)
+
+    
+    # setting config
+
+    return app
